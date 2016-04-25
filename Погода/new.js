9af7d468f2ac30d926f345d2dfa087b7b53c0e55
maxklimn <max_klimm@mail.ru>
2016-04-25 21:58:33 +0300

    погода

---
+++ Погода/new.js
@@ -0,0 +1,23 @@
+var elem = document.createElement('div');
+elem.className="panel-body"
+function getEl(){
+    var listCity = document.getElementById("inputCity");
+
+objCity.options[0]=new Option("London","London");
+objCity.options[1]=new Option("Kiev","Kiev");
+objCity.options[2]=new Option("Rome","Rome");
+objCity.options[3]=new Option("Berlin","Berlin");
+objCity.options[4]=new Option("Washington","Washington");
+objCity.options[5]=new Option("Vienna","Vienna");
+objCity.options[6]=new Option("Minsk","Minsk");
+objCity.options[7]=new Option("Baku","Baku");
+objCity.options[8]=new Option("Tbilisi","Tbilisi");
+var oOption = document.createElement("option");
+oOption.appendChild(document.createTextNode(text));
+oOption.setAttribute("value", value);}
+//var new_options = '';
+//$.each(newOptions, function(key, value) {
+ //   new_options += '' + value + '';
+//});$('#my_select').html(new_options);
+data sors
+col-xs-4
